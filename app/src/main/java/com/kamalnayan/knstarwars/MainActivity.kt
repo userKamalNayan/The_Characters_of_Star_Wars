4d035521f14b9975c89d13529925ba92d2c0b90b
kamalnayan <kamal.nayan@fantv.in>
2024-01-09 16:38:40 +0530

    Initial commit

---
+++ app/src/main/java/com/kamalnayan/knstarwars/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kamalnayan.knstarwars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
